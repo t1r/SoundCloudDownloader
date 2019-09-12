3182bae3193a677c0137aeb610e49aca9e55d635
Akeksandr Ermolenko <aleksander.ermolenko@simbirsoft.com>
2019-09-12 14:25:51 +0300

    initial commit

---
+++ app/src/main/java/com/t1r/scd/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.t1r.scd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.t1r.scd.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
